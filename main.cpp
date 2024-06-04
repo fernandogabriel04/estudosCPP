63e9277ce78faeee50fe98cefd49e2f5e94a7903
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2024-06-03 23:13:46 -0300

    docs: documenting hello world

--- main.cpp
+++ main.cpp
@@ -2,7 +2,11 @@
 
 int main() {
     std::cout << "ola mundo";
-    system("color 3");
+    // altera a cor do terminal
+    // 5 é a cor de fundo
+    // F é a cor da fonte
+    system("color 5F");
+    // executa o shell do Win e coloca o comando PAUSE
     system("PAUSE");
     return 0;
 }
